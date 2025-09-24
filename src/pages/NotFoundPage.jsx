45cfcd13992475e0dfd3ec7b3ff3917969b62214
Léo Maurice <l.maurice8361@gmail.com>
2025-09-24 17:57:29 +0200

    Ajout NotFoundPage

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+// change the title of the page
+document.title = "404 - Page Not Found";
+    return (
+        <div>
+            <h2>404 - Page Not Found</h2>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
+};
+export default NotFoundPage;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import AboutPage from './pages/AboutPage';
 import CharactersPage from './pages/CharactersPage';
 import ContactPage from './pages/ContactPage';
 import Layout from './Layout';
+import NotFoundPage from './pages/NotFoundPage';
 // routes of the application
 const routes = [
 {
@@ -23,6 +24,11 @@ const routes = [
         path: "/contact",
         Component: ContactPage
         },
+        {
+        // 404 page
+        path: "*",
+        Component: NotFoundPage
+        }
     ],
 },
 ]
