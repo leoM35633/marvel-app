7c145282bba113a8000eb14c15231e1aec9ab422
Léo Maurice <l.maurice8361@gmail.com>
2025-09-24 16:51:12 +0200

    Création du composant AboutPage

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+function AboutPage() {
+  document.title = 'About Us - Marvel App';
+  return (
+    <>
+        <h2>About Us</h2>
+        <p>We are a team of Marvel fan ...</p>
+
+    </>
+  );
+}
+
+export default AboutPage;
