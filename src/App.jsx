3c0d40a43ce5cf466103cf680e604aae72b126db
Léo Maurice <l.maurice8361@gmail.com>
2025-09-24 16:59:06 +0200

    Création composant ContactPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import './App.css';
 import characters from './data/characters.json';
 import CharactersList from './components/CharactersList'; // Import du composant
 import NumberOfCharacters from './components/NumberOfCharacters'
-import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <>
-      {/* <h1>Marvel Characters</h1>
+      <h1>Marvel Characters</h1>
       <CharactersList characters={characters} />
-      <NumberOfCharacters  characters={characters}/> */}
+      <NumberOfCharacters  characters={characters}/>
 
-       <AboutPage/>
+
       
 
     </>
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,14 @@
+function AboutPage() {
+  document.title = 'Contact - Marvel App';
+  return (
+    <>
+        <h2>Contact Us</h2>
+        <p>Fell free to contact us at  
+            <a  href="mailto:marvelApp@gamil.com"> marvelApp@gamil.com</a>
+        </p>
+
+    </>
+  );
+}
+
+export default AboutPage;
