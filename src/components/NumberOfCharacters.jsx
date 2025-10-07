122b807f43b299f57c9eb30496155272f0f859ab
Léo Maurice <l.maurice8361@gmail.com>
2025-10-07 17:46:25 +0200

    test: 🚨 update coverage reports and add tests for NumberOfCharacters component

--- src/components/NumberOfCharacters.jsx
+++ src/components/NumberOfCharacters.jsx
@@ -1,11 +1,14 @@
-function NumberOfCharacters({ characters = [] }) {
-  return (
-    <p>
-      {characters.length > 0
-        ? `There is ${characters.length} character${characters.length > 1 ? 's' : ''}`
-        : 'There is no character'}
-    </p>
-  );
-}
+/**
+ * NumberOfCharacters component
+ * @param {*} Array of characters, default is an empty array
+ * @returns 
+ */
+export default function NumberOfCharacters({ characters = [] }) {
+    // If no characters are provided or the array is empty
+    if (characters.length === 0) {
+      return <p>There is no character</p>;
+    }
 
-export default NumberOfCharacters;
+    // If there are characters in the array
+    return <p>There is {characters.length} characters</p>;
+}
